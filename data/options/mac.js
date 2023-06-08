fe41a80585bcd270fc29cf28b1c12422b8eef9ca
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-06-08 10:13:49 +0900

    api 연결

---
+++ data/options/mac.js
@@ -0,0 +1,332 @@
+const optionsMac = [
+  {
+    id: '1',
+    model: 'Mac Mini',
+    data: [
+      {
+        title: 'Mac Mini',
+        alias: 'Mac Mini',
+        imgSrc: '/static/images/macs/mac-mini.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2020',
+          cpu: 'M1 8코어 CPU 8코어 GPU',
+        },
+        options: [
+          {
+            id: 1,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 2,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 3,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 4,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 5,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 6,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Mac Mini',
+        alias: 'Mac Mini',
+        imgSrc: '/static/images/macs/mac-mini.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2022',
+          cpu: 'M2 8코어 CPU 10코어 GPU',
+        },
+        options: [
+          {
+            id: 7,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 8,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 9,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 10,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 11,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 12,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Mac Mini',
+        alias: 'Mac Mini',
+        imgSrc: '/static/images/macs/mac-mini.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2022',
+          cpu: 'M2 Pro 10코어 CPU 16코어 GPU',
+        },
+        options: [
+          {
+            id: 13,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 14,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+          {
+            id: 15,
+            ram: '32GB',
+            ssd: '512GB',
+          },
+          {
+            id: 16,
+            ram: '32GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Mac Mini',
+        alias: 'Mac Mini',
+        imgSrc: '/static/images/macs/mac-mini.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2022',
+          cpu: 'M2 Pro 12코어 CPU 19코어 GPU',
+        },
+        options: [
+          {
+            id: 17,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 18,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+          {
+            id: 19,
+            ram: '32GB',
+            ssd: '512GB',
+          },
+          {
+            id: 20,
+            ram: '32GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '2',
+    model: 'Macbook Air 13',
+    data: [
+      {
+        title: 'Macbook Air 13',
+        alias: 'Macbook Air 13',
+        imgSrc: '/static/images/macs/macbook-air-13-2020.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2020',
+          cpu: 'M1 8코어 CPU 7코어 GPU',
+        },
+        options: [
+          {
+            id: 1,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 2,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 3,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 4,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 5,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 6,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Macbook Air 13',
+        alias: 'Macbook Air 13',
+        imgSrc: '/static/images/macs/macbook-air-13-2020.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2020',
+          cpu: 'M1 8코어 CPU 8코어 GPU',
+        },
+        options: [
+          {
+            id: 7,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 8,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 9,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 10,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 11,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 12,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Macbook Air 13',
+        alias: 'Macbook Air 13',
+        imgSrc: '/static/images/macs/macbook-air-13-2022.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2022',
+          cpu: 'M2 8코어 CPU 8코어 GPU',
+        },
+        options: [
+          {
+            id: 13,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 14,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 15,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 16,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 17,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 18,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+      {
+        title: 'Macbook Air 13',
+        alias: 'Macbook Air 13',
+        imgSrc: '/static/images/macs/macbook-air-13-2022.jpeg',
+        href: 'https://www.google.com',
+        specs: {
+          year: '2022',
+          cpu: 'M2 8코어 CPU 10코어 GPU',
+        },
+        options: [
+          {
+            id: 19,
+            ram: '8GB',
+            ssd: '256GB',
+          },
+          {
+            id: 20,
+            ram: '8GB',
+            ssd: '512GB',
+          },
+          {
+            id: 21,
+            ram: '8GB',
+            ssd: '1TB',
+          },
+          {
+            id: 22,
+            ram: '16GB',
+            ssd: '256GB',
+          },
+          {
+            id: 23,
+            ram: '16GB',
+            ssd: '512GB',
+          },
+          {
+            id: 24,
+            ram: '16GB',
+            ssd: '1TB',
+          },
+        ],
+      },
+    ],
+  },
+]
+
+export default optionsMac
--- pages/prices/mac/[model].js
+++ pages/prices/mac/[model].js
@@ -1,21 +1,75 @@
 import { PageSEO } from '@/components/SEO'
-import mackbookAir13Data from '@/data/macs/macbook-air-13'
+
 import Image from 'next/image'
 
 import Chart from 'chart.js/auto'
 import { Line } from 'react-chartjs-2'
+import optionsMac from '@/data/options/mac'
+import { useEffect, useRef, useState } from 'react'
+import axiosInstance from '@/lib/axios'
+import useAsync from 'hooks/useAsync'
+
+async function getPrices(itemId = 1, optionId = 1) {
+  const response = await axiosInstance.get(`/item/${itemId}/option/${optionId}`)
+  return response.data
+}
 
 const MacModel = ({ model }) => {
-  // 모델명 대문자로 변경 ex) macbook-air-13 -> Macbook Air 13
-  let modelTitle = model
-    .split('-')
-    .map((w) => w[0].toUpperCase() + w.slice(1))
-    .join(' ')
+  let currentItem = null
+
+  switch (model) {
+    case 'mac-mini':
+      currentItem = optionsMac.find((m) => m.id === '1')
+      break
+    case 'macbook-air-13':
+      currentItem = optionsMac.find((m) => m.id === '2')
+      break
+    default:
+      currentItem = optionsMac.find((m) => m.id === '1')
+  }
+
+  const { data: modelData } = currentItem
+  const [currentModel, setCurrentModel] = useState(modelData[0])
+
+  const { title: modelTitle, specs, options, imgSrc, href } = currentModel
+  const [currentOption, setCurrentOption] = useState(options[0])
+  const { ram, ssd } = currentOption
+
+  // 가격 조회
+  const [state, refetch] = useAsync(getPrices, [])
+  const { loading, data: fetchedData, error } = state
+
+  if (loading) return <div>로딩중..</div>
+  if (error) return <div>에러가 발생했습니다</div>
+  if (!fetchedData) return null
+
+  const { data: prices } = fetchedData
+
+  console.log(fetchedData)
+
+  const onInputOptionCPU = (optionIndex) => {
+    // cpu에 맞는 모델로 변경
+    setCurrentModel(modelData[optionIndex])
+
+    // 옵션 초기화
+    setCurrentOption(modelData[optionIndex].options[0])
+  }
+
+  const onChangeOption = async (optionId) => {
+    // ram, ssd에 맞는 모델로 변경
+    setCurrentOption(options.find((option) => option.id === optionId))
+
+    try {
+      refetch()
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <>
       <PageSEO
-        title={`맥 시세 | ${modelTitle}`}
+        title={`맥 시세 | ${specs.year} ${modelTitle}`}
         description={`ChatGPT가 알려주는 사양별 맥 시세 | ${modelTitle}`}
       />
 
@@ -23,9 +77,9 @@ const MacModel = ({ model }) => {
         <div className="flex flex-col md:flex-row">
           <div className="w-full md:w-1/3">
             <Image
-              alt={'hello'}
-              src={'/static/images/macbook-air.jpeg'}
-              className="object-cover object-center md:h-36 lg:h-48"
+              alt={`${specs.year} ${modelTitle} 이미지`}
+              src={imgSrc}
+              className="object-contain object-center md:h-36 lg:h-48"
               width={544}
               height={306}
             />
@@ -74,7 +128,7 @@ const MacModel = ({ model }) => {
 
           <div className="flex-grow md:px-3">
             <h1 className="text-xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-2xl">
-              Apple {modelTitle} ({'12GB' + ', ' + 'SSD ' + '256GB'})
+              {modelTitle} ({`${ram}, SSD ${ssd}`})
             </h1>
 
             <div className="max-w-xl">
@@ -91,7 +145,12 @@ const MacModel = ({ model }) => {
                       </p>
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      $320
+                      {
+                        // get last index of prices
+
+                        // prices[prices.length - 1].mid
+                        '$1234'
+                      }
                     </div>
                   </div>
                 </li>
@@ -134,125 +193,52 @@ const MacModel = ({ model }) => {
               <div className="mt-3">
                 <div className="w-full max-w-md">
                   <label
-                    htmlFor="countries"
+                    htmlFor="cpuOptions"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
                     CPU
                   </label>
                   <select
-                    id="countries"
+                    id="cpuOptions"
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                    onInput={(e) => onInputOptionCPU(e.target.value)}
+                    value={modelData.indexOf(currentModel)}
                   >
-                    <option selected value="S급">
-                      M1 (8코어 CPU, 8코어 GPU)
-                    </option>
-                    <option value="미개봉">미개봉</option>
+                    {modelData.map((model, index) => (
+                      <option key={model.specs.cpu} value={index}>
+                        {model.specs.cpu} ({model.specs.year})
+                      </option>
+                    ))}
                   </select>
                 </div>
 
                 <ul className="mt-3 flex w-full  max-w-xl flex-wrap items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0  md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-license"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-license"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 8GB, SSD 256GB
-                      </label>
-                    </div>
-                  </li>
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-id"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-id"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 8GB, SSD 512GB
-                      </label>
-                    </div>
-                  </li>
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-id"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-id"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 16GB, SSD 256GB
-                      </label>
-                    </div>
-                  </li>
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-id"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-id"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 16GB, SSD 512GB
-                      </label>
-                    </div>
-                  </li>
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-id"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-id"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 16GB, SSD 1TB
-                      </label>
-                    </div>
-                  </li>
-                  <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 md:w-1/2">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id="horizontal-list-radio-id"
-                        type="radio"
-                        value=""
-                        name="list-radio"
-                        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />
-                      <label
-                        htmlFor="horizontal-list-radio-id"
-                        className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        RAM 8GB, SSD 256GB
-                      </label>
-                    </div>
-                  </li>
+                  {options.map((option) => (
+                    <li
+                      key={option.id}
+                      className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0  md:w-1/2"
+                    >
+                      <div className="flex items-center pl-3">
+                        <input
+                          onChange={() => {
+                            onChangeOption(option.id)
+                          }}
+                          id={option.id}
+                          checked={currentOption === option}
+                          type="radio"
+                          value=""
+                          name="list-radio"
+                          className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
+                        />
+                        <label
+                          htmlFor={option.id}
+                          className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                          {`RAM ${option.ram}, SSD ${option.ssd}`}
+                        </label>
+                      </div>
+                    </li>
+                  ))}
                 </ul>
 
                 <div className="grid max-w-md grid-cols-2 gap-2 py-5">
@@ -319,7 +305,7 @@ const MacModel = ({ model }) => {
 }
 
 export async function getServerSideProps(context) {
-  const { model, year, ram, ssd } = context.query
+  const { model } = context.query
 
   return {
     props: {

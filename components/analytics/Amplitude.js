aa47f2b3b145d5585ea7dd2b49dba4b2d08a2bac
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-06-10 14:41:05 +0900

    amplitude 테스트

---
+++ components/analytics/Amplitude.js
@@ -0,0 +1,22 @@
+// import Script from 'next/script'
+
+// import siteMetadata from '@/data/siteMetadata'
+// import { Analytics } from 'analytics'
+// import amplitudePlugin from '@analytics/amplitude'
+
+// const Amplitude = Analytics({
+//   app: 'macguider',
+//   plugins: [
+//     // amplitudePlugin
+//     amplitudePlugin({
+//       apiKey: '6f840da4543c36607e9d84527bf079c3',
+//       // eventPrefix: 'myapp',
+//       // userId: '123',
+//       // config: {
+//       //     // optional configuration for analytics amplitude SDK
+//       // }
+//     }),
+//   ],
+// })
+
+// export default Amplitude
--- pages/_app.js
+++ pages/_app.js
@@ -14,12 +14,28 @@ import { ClientReload } from '@/components/ClientReload'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Router } from 'next/router'
+import * as amplitude from '@amplitude/analytics-node'
+
 config.autoAddCss = false
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
+  // Router.events.on('routeChangeComplete', () => {
+  //   console.log('routeChangeComplete')
+  //   Amplitude.page()
+  // })
+
+  amplitude.init('6f840da4543c36607e9d84527bf079c3', null, {
+    includeGclid: true,
+    includeUtm: true,
+    includeReferrer: true,
+  })
+
+  amplitude.track('page_view', { page_type: 'main', page_detail: 'main' })
+
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>

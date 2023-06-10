680f445712c07631e5fded9c5c6d4a43baec2591
Seungjik Lee <lsjbh45@gmail.com>
2023-06-10 16:01:00 +0900

    chore: Add amplitude-js

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,7 @@ import { ClientReload } from '@/components/ClientReload'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { Router } from 'next/router'
-import * as amplitude from '@amplitude/analytics-node'
+import amplitude from 'amplitude-js'
 
 config.autoAddCss = false
 
@@ -23,18 +22,8 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
-  // Router.events.on('routeChangeComplete', () => {
-  //   console.log('routeChangeComplete')
-  //   Amplitude.page()
-  // })
-
-  amplitude.init('6f840da4543c36607e9d84527bf079c3', null, {
-    includeGclid: true,
-    includeUtm: true,
-    includeReferrer: true,
-  })
-
-  amplitude.track('page_view', { page_type: 'main', page_detail: 'main' })
+  amplitude.getInstance().init('6f840da4543c36607e9d84527bf079c3')
+  amplitude.getInstance().logEvent('test')
 
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>

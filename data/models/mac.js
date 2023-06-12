1a885c59af57c0e003e5d2c00f4d20cf8d4ea6cc
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-06-12 20:46:13 +0900

    MacBook 이름 변경

--- data/models/mac.js
+++ data/models/mac.js
@@ -1,21 +1,21 @@
 const macModels = [
   {
-    title: 'Macbook Pro 16',
+    title: 'MacBook Pro 16',
     imgSrc: '/static/images/macs/macbook-pro-16-2023.jpeg',
     href: '/prices/mac/macbook-pro-16',
   },
   {
-    title: 'Macbook Pro 14',
+    title: 'MacBook Pro 14',
     imgSrc: '/static/images/macs/macbook-pro-14-2023.jpeg',
     href: '/prices/mac/macbook-pro-14',
   },
   {
-    title: 'Macbook Pro 13',
+    title: 'MacBook Pro 13',
     imgSrc: '/static/images/macs/macbook-pro-13-2022.jpeg',
     href: '/prices/mac/macbook-pro-13',
   },
   {
-    title: 'Macbook Air 13',
+    title: 'MacBook Air 13',
     imgSrc: '/static/images/macs/macbook-air-13-2022.jpeg',
     href: '/prices/mac/macbook-air-13',
   },

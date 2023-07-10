907a132bf487b184245273e232da0ae35b89ff05
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-07-10 09:43:38 +0900

    fix: 가격 상세 페이지 이벤트 추가

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/prices', title: '시세 정보' },
   { href: '/buyers-guide', title: '구매 가이드' },
   { href: '/desk', title: '오늘의 데스크' },
+  { href: 'https://tally.so/r/mYRpxz', title: '피드백 제공' },
   // { href: '/blog', title: '블로그' },
 ]
 

991ed20769b0ce1aed3666c247907487c4277063
Seungjik Lee <lsjbh45@gmail.com>
2023-07-17 12:39:54 +0900

    feat: Add admin page for deal

--- utils/deals.js
+++ utils/deals.js
@@ -4,3 +4,18 @@ export async function getDeals() {
   const response = await axiosInstanceV2.get(`/deal`)
   return response.data
 }
+
+export async function getDealRaw(id) {
+  const response = await axiosInstanceV2.get(`/deal/raw/${id}`)
+  return response.data
+}
+
+export async function getItems() {
+  const response = await axiosInstanceV2.get(`/item`)
+  return response.data
+}
+
+export async function convertDealFromRaw(id, payload) {
+  const response = await axiosInstanceV2.put(`/deal/raw/${id}`, payload)
+  return response.data
+}

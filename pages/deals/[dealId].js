318600673ed0e4a793191afaea4398093c8d9cab
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-07-30 20:56:31 +0900

    feat: 인증 deals 게시

--- pages/deals/[dealId].js
+++ pages/deals/[dealId].js
@@ -16,6 +16,7 @@ import { pastTime } from '@/lib/utils/pastTime'
 import Banner from '@/components/Banner'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { useQuery, useQueries } from 'react-query'
+import DealCard from '@/components/deals/DealCard'
 
 const rightColumnOffsetY = 112
 const numberOfSampleDevices = 6
@@ -55,7 +56,7 @@ export default function Deal({ dealId }) {
     data: deals,
   } = useQuery(
     ['deal', 'other_deals', dealId, deal],
-    () => getDeals(1, 4, 'date', 'desc', deal.item.type, deal.item.model.id),
+    () => getDeals(1, 4, 'date', 'desc', deal?.item.type, deal?.item.model.id),
     {
       enabled: deal && Object.keys(deal).length > 0,
     }
@@ -259,12 +260,6 @@ export default function Deal({ dealId }) {
     }
   }, [priceInfo])
 
-  const getDiscountPercentage = useCallback((price, avgPrice) => {
-    if (avgPrice) {
-      return Math.round((1 - price / avgPrice) * 100)
-    }
-  }, [])
-
   return (
     <>
       <PageSEO title={`중고 핫딜`} description={`매일 중고 꿀매를 대신 찾아드립니다`} />
@@ -432,72 +427,12 @@ export default function Deal({ dealId }) {
                     .filter((deal) => deal.id !== Number(dealId))
                     .sort(() => Math.random() - Math.random())
                     .slice(0, 3)
-                    .map(({ id: dealId, source, price, sold, unused, item, average }) => (
-                      <div
-                        onClick={() => {
-                          onClickOtherDeal(dealId)
-                        }}
-                        className="flex h-[120px] w-full cursor-pointer items-center overflow-hidden  bg-white"
-                        key={dealId}
-                      >
-                        <div className="mr-2 flex-1 truncate">
-                          <div className="mt-1 truncate  text-base  font-medium tracking-tight text-gray-600">
-                            <span className="mr-1 inline-block font-semibold">
-                              {unused ? (
-                                <span className="text-blue-500">미개봉</span>
-                              ) : (
-                                <span className="text-green-500">S급</span>
-                              )}
-                            </span>
-                            {item.type === 'M'
-                              ? `${item.model.name} ${item.details.chip}`
-                              : `${item.model.name} ${item.details.gen}세대`}
-                          </div>
-                          <div className="truncate text-xs font-normal text-gray-500">
-                            <span className="mr-1 inline-block  text-gray-600">{source}</span>
-                            {item.type === 'M'
-                              ? `CPU ${item.details.cpu} GPU ${item.details.gpu}, RAM ${item.details.ram}GB, SSD ${item.details.ssd}`
-                              : `${item.details.cellular ? 'Wi-Fi + Cellular' : 'Wi-Fi'} (${
-                                  item.details.storage
-                                })`}
-                          </div>
-
-                          <div className=" flex items-center text-lg">
-                            <div className="font-bold text-gray-900">
-                              {price?.toLocaleString()}원
-                            </div>
-                          </div>
-
-                          {average && (
-                            <div className="text-xs  text-gray-500">
-                              <span className="font-semibold text-blue-500">
-                                {getDiscountPercentage(price, average)}%&nbsp;
-                              </span>
-                              <span>평균&nbsp;</span>
-                              {average?.toLocaleString()}원
-                            </div>
-                          )}
-                        </div>
-
-                        <div className="flex h-full w-1/4 max-w-[100px] items-center">
-                          <div className="relative aspect-1 overflow-hidden rounded-md">
-                            <img
-                              src={`${process.env.NEXT_PUBLIC_API_URL_V2}/deal/${dealId}/image`}
-                              alt={`${item.model.name} 썸네일`}
-                              className="h-full w-full object-cover object-center"
-                            />
-
-                            {sold && (
-                              <div className="absolute top-0  left-0 flex h-full w-full items-center justify-center text-sm font-bold text-white ">
-                                <div className="absolute top-0 left-0 h-full w-full bg-black opacity-40" />
-                                <div className="absolute top-0 left-0 flex h-full w-full items-center justify-center">
-                                  판매완료
-                                </div>
-                              </div>
-                            )}
-                          </div>
-                        </div>
-                      </div>
+                    .map((deal) => (
+                      <DealCard
+                        deal={deal}
+                        key={deal.id}
+                        clickHandler={() => onClickOtherDeal(deal.id)}
+                      />
                     ))}
             </div>
           </div>

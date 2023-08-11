bade8da39bb66f9414f2e2518aa873794be6ded4
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-08-11 09:04:55 +0900

    fix: init amplitude with userId

--- components/analytics/Amplitude.js
+++ components/analytics/Amplitude.js
@@ -5,24 +5,24 @@ import { useCookies } from 'react-cookie'
 import { Base64 } from 'js-base64'
 
 const Amplitude = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(['refreshToken'])
+  const [cookies] = useCookies(['refreshToken'])
   const refreshToken = cookies['refreshToken']
   const { data: accessToken } = useQuery('accessToken', () => {})
 
   useEffect(() => {
     const isProduction = () => process.env.NODE_ENV === 'production'
     const isBrowser = () => typeof window !== 'undefined'
-    let userEmail = null
+    let userId = null
 
     if (isBrowser()) {
       if (refreshToken && accessToken) {
         const decoded = Base64.decode(refreshToken)
-        const emailRegex = /"email":"([^"]+)"/
-        userEmail = decoded.match(emailRegex)[1]
+        const idRegex = /"id":(\d+)/
+        userId = decoded.match(idRegex)[1]
       }
 
       if (isProduction()) {
-        init(process.env.NEXT_PUBLIC_AMPLITUDE_ID, userEmail, {
+        init(process.env.NEXT_PUBLIC_AMPLITUDE_ID, userId, {
           defaultTracking: { pageViews: false },
           includeFbclid: true,
           includeGclid: true,

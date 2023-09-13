0b66a335e799232c9a8dce798c7554cee827855e
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-09-13 17:44:03 +0900

    fix: Resolved duplicated categories

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,10 +1,27 @@
 import { useRouter } from 'next/router'
 import Link from './Link'
-import headerNavLinks from '@/data/headerNavLinks'
+import headerNavLinks, { HeaderNavLink } from '@/data/headerNavLinks'
 import MobileNav from './MobileNav'
+import { useCallback } from 'react'
 
 const Navigation = () => {
   const router = useRouter()
+
+  const isCurrentCategory = useCallback(
+    (link: HeaderNavLink) => {
+      if (router.pathname.startsWith('/prices') && link.href === '/') {
+        return true
+      }
+
+      if (link.exact) {
+        return router.pathname === link.href
+      }
+
+      return router.pathname.startsWith(link.href)
+    },
+    [router.pathname]
+  )
+
   return (
     <div className="flex items-center text-base leading-5">
       <div className="hidden xl:block">
@@ -13,13 +30,7 @@ const Navigation = () => {
             key={link.title}
             href={link.href}
             className={`p-1 font-semibold ${
-              link.exact
-                ? router.pathname === link.href
-                  ? 'text-black'
-                  : 'text-gray-500'
-                : router.pathname.startsWith(link.href)
-                ? 'text-black'
-                : 'text-gray-500'
+              isCurrentCategory(link) ? 'text-black' : 'text-gray-500'
             } sm:p-4`}
           >
             {link.title}
